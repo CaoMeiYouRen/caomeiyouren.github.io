a411b0e0ed0db9aa07e0c644af8146eb0645bc66
CaoMeiYouRen <996881204@qq.com>
2024-08-02 22:09:51 +0800

    feat: 新增 vitepress 构建

---
+++ .vitepress/config.mjs
@@ -0,0 +1,12 @@
+export default {
+    // 站点级选项
+    title: 'CaoMeiYouRen',
+    description: '草梅友仁的个人介绍',
+
+    themeConfig: {
+        // 主题级选项
+    },
+    rewrites: {
+        'README.md': 'index.md'
+    }
+}
